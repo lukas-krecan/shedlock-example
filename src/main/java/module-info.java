f5893590082ee792afdf9ec05cc560a330963176
Lukas Krecan <lukas@krecan.net>
2017-09-23 10:10:28 +0200

    Requires added

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,2 +1,10 @@
 module shedlock.example {
+    requires spring.context;
+    requires spring.jdbc;
+    requires slf4j.api;
+    requires shedlock.core;
+    requires shedlock.spring;
+    requires HikariCP;
+    requires shedlock.provider.jdbc.template;
+    requires java.sql;
 }
